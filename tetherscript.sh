dd586fe80378ac50b8ea1896cdbcff3b7e6a3918
tetris <tetris11>
2016-08-09 15:27:06 +0100

    Almost there

--- tetherscript.sh
+++ tetherscript.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-function tether(){
+#function tether(){
 	# Downlink ethernet
-	enp=`ip a | grep -oP "(enp[0-9]s[^ ]*)[0-9]"`
+	enp=`ip a | grep -oP "(enp[1-9]s[^ ]*)[0-9]"`
 	echo "set $enp down"
-	sudo ip link set dev $enp  down
+	sudo ip link set dev $enp down
 
 	# Connects to the latest usb device
 	dev=`ip a | grep -oP "(usb|enp0s[^ ]*)[0-9]"`
 	echo "listen on $dev"
 	sudo dhcpcd $dev
 	ping 8.8.8.8
-}
+#}
