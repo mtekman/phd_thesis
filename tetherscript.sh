79f82b6be16051d34e713a9e1e0e64be99d3179a
tetris <tetris11>
2016-08-11 17:51:47 +0100

    default routing stead

--- tetherscript.sh
+++ tetherscript.sh
@@ -2,13 +2,14 @@
 
 #function tether(){
 	# Downlink ethernet
-	enp=`ip a | grep -oP "(enp[1-9]s[^ ]*)[0-9]"`
-	echo "set $enp down"
-	sudo ip link set dev $enp down
+#	enp=`ip a | grep -oP "(enp[1-9]s[^ ]*)[0-9]"`
+#	echo "set $enp down"
+#	sudo ip link set dev $enp down
 
 	# Connects to the latest usb device
 	dev=`ip a | grep -oP "(usb|enp0s[^ ]*)[0-9]"`
 	echo "listen on $dev"
 	sudo dhcpcd $dev
+	sudo ip route del default via 192.168.1.1
 	ping 8.8.8.8
 #}
