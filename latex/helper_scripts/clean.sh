68035b80b8afc5a8840484a4e30727d6f9492a48
tetris <tetris11>
2016-08-15 16:14:00 +0100

    main removers

--- latex/helper_scripts/clean.sh
+++ latex/helper_scripts/clean.sh
@@ -5,5 +5,6 @@ mp=../
 res1=`find $mp -type f -name "*.aux"`
 res2=`find $mp -type f -name "*.a*"`
 res3=`find $mp -type f -name "*.*-gl*"`
+res4=`find ../ -maxdepth 1 -type f -name "Main.*" -a ! -name "Main.tex" -a ! -name "Main.pdf"`
 
-echo $res1 $res2 $res3 | tr " " "\n"
+echo $res1 $res2 $res3 $res4 | tr " " "\n" |  sort | uniq
