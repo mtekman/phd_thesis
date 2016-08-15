6ff8902da0b3c061b624e7325b84b8095eff5729
tetris <tetris11>
2016-08-15 18:21:52 +0100

    latest

--- latex/helper_scripts/printingcostanalysis/pagecoloranalysis.sh
+++ latex/helper_scripts/printingcostanalysis/pagecoloranalysis.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-gs -o - -sDEVICE=inkcov ../../Main.pdf > cmyk_per_page.txt 
-cat cmyk_per_page.txt\
+gs -o - -sDEVICE=inkcov ../../mtekman_phd_thesis.v4.2016_08_15.printed.pdf | tee temp.txt
+cat temp.txt\
  | tr "\n" "  "\
  | sed 's/Page/\nPage/g'\
  | grep -v "GPL"\
- > cmyk_per_page.2.txt
+ > cmyk_per_page.txt
+
+rm temp.txt
